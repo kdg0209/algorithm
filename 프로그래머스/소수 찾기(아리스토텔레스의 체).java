9fb05ca25324fcb9fc5375b44fafe57673d31da1
KDG-IS <80187200+kdg0209@users.noreply.github.com>
2022-04-01 20:43:19 +0900

    Update 소수 찾기(아리스토텔레스의 체).java

--- 프로그래머스/소수 찾기(아리스토텔레스의 체).java
+++ 프로그래머스/소수 찾기(아리스토텔레스의 체).java
@@ -6,7 +6,9 @@ class Solution {
         for (int i = 2; i <= n; i++) {
             boolean isPrime = true;
           
-            // 2부터 
+            // 2부터 배수로 찾기
+            // 4, 5, 6, 7, 8, 9, 10, 11, 12, ...
+            // 4, 9, 16, 25, 36, 49, 64, 81, ...
             for (int j = 2; j * j <= i; j++) {
                 if (i % j == 0) {
                     isPrime = false;
